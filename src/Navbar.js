868796adea42c029c58135179d470ef1832323dc
Lilydahamna <liliadahamna2000@gmail.com>
2023-07-03 01:30:37 -0400

    another fix

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const  Navbar = () => {
     return (  
         <nav className="navbar navbar-expand-md bg-pink">
         <div className="container-fluid">
-           <a className="navbar-brand navbar-heading" href="/">Brains&Beauty</a>
+           <Link className="navbar-brand navbar-heading" to="/">Brains&Beauty</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
